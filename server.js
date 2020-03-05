00ccf8d62df61942f0550489443f1bd3d82b3fb2
Ben F <56423163+bferriman@users.noreply.github.com>
2020-03-05 11:33:08 -0500

    basic server implemention in place

---
+++ server.js
@@ -0,0 +1,29 @@
+// Requiring necessary npm packages
+var express = require("express");
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+// require projects data json file
+
+// Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 8080;
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  console.log("GET request received");
+  const projects = {};
+  res.render("index", projects);
+});
+
+// Syncing our database and logging a message to the user upon success
+app.listen(PORT, function() {
+  console.log("==> Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+});
+
