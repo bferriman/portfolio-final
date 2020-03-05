8736edfd7cbb21a80d070fd9721cd81fb5f2c937
Ben F <56423163+bferriman@users.noreply.github.com>
2020-03-05 11:49:05 -0500

    added new projects and an order attribute

--- projects.js
+++ projects.js
@@ -1,17 +1,38 @@
 const projects = [
-{
+  {
+    name: "Friendango",
+    order: 1,
+    description: "No clout, just friends.  Read movie reviews from your friends without the distraction of chasing social media followers.",
+    screenshot: "/images/friendango.jpg"
+  },  
+  {
     name: "PetSpiration",
+    order: 2,
     description: "Are you ready to give up on human companionship but not the fun of a dating app? Then PetSpiration is for you! We'll show you pictures of cats, you tell us whether you like them. Then we'll match you up with adoptable cats near you!",
-    screenshot: "../images/petspiration.jpg"
-},
-{
+    screenshot: "/images/petspiration.jpg"
+  },
+  {
     name: "Day Planner",
+    order: 3,
     description: "Time to get organized! This app lets you schedule and save events. Uses Moment.js to responsively format time and date, and saves your events in Local Storage. Uses DOM manipulation to display state-appropriate button controls.",
-    screenshot: "../images/dayplanner.jpg"
-},
-{
+    screenshot: "/images/dayplanner.jpg"
+  },
+  {
     name: "Weather Dashboard",
+    order: 4,
     description: "Get the current weather as well as the five day forecast, for wherever you are, or wherever you may be going! Uses the OpenWeather API.",
-    screenshot: "../images/weather.jpg"
-}
+    screenshot: "/images/weather.jpg"
+  },
+  {
+    name: "Eat-The-Burger",
+    order: 5,
+    description: "A silly app, but a great way to practice MVC implementation. Create a burger, then eat it! Uses MySQL, Handlebars, Express, and a custom-built ORM.",
+    screenshot: "/images/eat-the-burger.jpg"
+  },
+  {
+    name: "Employee Template",
+    order: 6,
+    description: "A Node CLI for entering employee data for a software development team.  When done, it generates a styled HTML page to display your team.",
+    screenshot: "/images/emp-template.jpg"
+  }
 ]
