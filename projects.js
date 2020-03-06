3b45053545c319e4d0860d7e637ee4c61ca97ac0
Ben F <56423163+bferriman@users.noreply.github.com>
2020-03-06 01:56:56 -0500

    changed project order

--- projects.js
+++ projects.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     {
       name: "Day Planner",
-      order: 3,
+      order: 4,
       description: "Time to get organized! This app lets you schedule and save events. Uses Moment.js to responsively format time and date, and saves your events in Local Storage. Uses DOM manipulation to display state-appropriate button controls.",
       screenshot: "/images/dayplanner.jpg",
       deployedURL: "https://bferriman.github.io/day-planner/",
@@ -26,7 +26,7 @@ module.exports = {
     },
     {
       name: "Weather Dashboard",
-      order: 4,
+      order: 3,
       description: "Get the current weather as well as the five day forecast, for wherever you are, or wherever you may be going! Uses the OpenWeather API.",
       screenshot: "/images/weather.jpg",
       deployedURL: "https://bferriman.github.io/weather-dashboard/",
@@ -34,7 +34,7 @@ module.exports = {
     },
     {
       name: "Eat-The-Burger",
-      order: 5,
+      order: 6,
       description: "A silly app, but a great way to practice MVC implementation. Create a burger, then eat it! Uses MySQL, Handlebars, Express, and a custom-built ORM.",
       screenshot: "/images/eat-the-burger.jpg",
       deployedURL: "https://limitless-ocean-97488.herokuapp.com/",
@@ -42,7 +42,7 @@ module.exports = {
     },
     {
       name: "Employee Template",
-      order: 6,
+      order: 5,
       description: "A Node CLI for entering employee data for a software development team.  When done, it generates a styled HTML page to display your team.",
       screenshot: "/images/emp-template.jpg",
       deployedURL: "https://github.com/bferriman/employee-template",
